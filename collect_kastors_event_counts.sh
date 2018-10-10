3120eee2a750e6833d05f9bb9a054d89ace01764
Dylan Chapp <chapp1@llnl.gov>
2018-10-10 12:54:03 -0700

    Changed result storage

--- collect_kastors_event_counts.sh
+++ collect_kastors_event_counts.sh
@@ -3,32 +3,37 @@
 n_trials=$1
 
 # Apps and inputs
-app_dir="${HOME}/repos/kastors-1.1/build_event_counter_tool/bin"
+kastors_bin_dir="${HOME}/repos/kastors-1.1/build_event_counter_tool/bin"
 apps=("sparselu_task"
       "sparselu_taskdep"
       "strassen_task"
       "strassen_taskdep"
      )
-#thread_counts=(2 4 6 8 10 12 14 16 18 20 22 24)
 thread_counts=(1 2 4 8 16 32)
 
 # Set up result storage
 result_dir="${HOME}/repos/OMPT_Testing/kastors_event_counts/"
 mkdir -p ${result_dir}
 
-cd ${app_dir}
-for t in ${thread_counts[@]}
-do
-    echo "Working on ${t}-thread cases"
-    export OMP_NUM_THREADS=$t
-    for app in ${apps[@]}
+cd ${kastors_bin_dir}
+for app in ${apps[@]}
+do 
+    app_result_dir="${result_dir}/${app}/"
+    mkdir -p ${app_result_dir}
+    echo "Working on application: ${app}"
+    
+    for t in ${thread_counts[@]}
     do
-        echo "Working on application: ${app}"
+        echo "Working on ${t}-thread cases"
+        export OMP_NUM_THREADS=$t
+        
         for trial_idx in `seq -s " " -f %04g ${n_trials}`
         do
             echo "Executing trial: ${trial_idx}"
-            ./${app} >& "${result_dir}/${app}_nthreads${t}_trial${trial_idx}.txt"
+            ./${app} >& "${app_result_dir}/nthreads${t}_trial${trial_idx}.txt"
         done 
+
     done
+
 done
 
