507c2670c8a6559b6aa945e921c5779468661a8b
Dylan Chapp <chapp1@llnl.gov>
2018-10-16 13:47:38 -0700

    Renaming

--- build_scripts/build_ompt_tools.sh
+++ build_scripts/build_ompt_tools.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash 
 
 tools_root="$HOME/repos/ompt_tools"
-dagtool_root="${tools_root}/dagtool"
-eventcounter_root="${tools_root}/event_counter" 
 skeleton_root="${tools_root}/skeleton" 
+event_counter_root="${tools_root}/event_counter" 
+ancestry_tracker_root="${tools_root}/ancestry_tracker" 
+dependency_tracker_root="${tools_root}/dependency_tracker"
 
 # Build the skeleton tool
 echo "Building skeleton OMPT tool"
@@ -13,12 +14,18 @@ make
 
 # Build the event-counter tool
 echo "Building event-counting OMPT tool"
-cd ${eventcounter_root}/src
+cd ${event_counter_root}/src
 make clean
 make
 
-# Build the full dependency analysis tool
+# Build the ancestry tracker tool
+echo "Building ancestry-tracking OMPT tool"
+cd ${ancestry_tracker_root}/src
+make clean
+make
+
+# Build the dependency analysis tool
 echo "Building full dependency analysis OMPT tool"
-cd ${dagtool_root}/src
+cd ${dependency_tracker_root}/src
 make clean
 make 
