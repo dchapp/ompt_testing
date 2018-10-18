a371c28c61400b5bc8d084899ea09d33ed19c3f4
Dylan Chapp <chapp1@llnl.gov>
2018-10-18 10:48:04 -0700

    Updated reproducer for event count data

--- reproducers/reproduce_kastors_event_counts.sh
+++ reproducers/reproduce_kastors_event_counts.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash 
 
 n_trials=10
+project_root="${HOME}/repos/OMPT_Testing"
 
-../build_scripts/build_event_counter_tool.sh
-../build_scripts/build_kastors_event_counter.sh
-../job_scripts/collect_kastors_event_counts.sh ${n_trials} 
+${project_root}/build_scripts/build_event_counter_tool.sh
+${project_root}/build_scripts/build_kastors_event_counter.sh
+${project_root}/job_scripts/collect_kastors_event_counts.sh ${n_trials} 
 
-data=../results/kastors/event_counts/
+data=${project_root}/results/kastors/event_counts/
 for dir in "${data}"/[^archive]*/
 do
     echo "Generating event-count plot for ${dir}"
-    ../plotting_scripts/make_event_count_plots.py ${dir}
+    ${project_root}/plotting_scripts/make_event_count_plots.py ${dir}
 done
 
-../cleanup_scripts/archive_kastors_event_count_data.sh
-../cleanup_scripts/clean_kastors.sh
-../cleanup_scripts/clean_kastors_event_count_data.sh
+${project_root}/cleanup_scripts/archive_kastors_event_count_data.sh
+${project_root}/cleanup_scripts/clean_kastors.sh
+${project_root}/cleanup_scripts/clean_kastors_event_count_data.sh
