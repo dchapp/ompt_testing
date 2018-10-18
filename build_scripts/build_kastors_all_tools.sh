dfc2d50e50dc774398f668ea9767fa62197164e5
Dylan Chapp <chapp1@llnl.gov>
2018-10-18 10:45:59 -0700

    Updated scripts for building all OMPT tools and KASTORS apps with all OMPT tools linked

--- build_scripts/build_kastors_all_tools.sh
+++ build_scripts/build_kastors_all_tools.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+project_root="${HOME}/repos/OMPT_Testing"
 kastors_dir="/g/g17/chapp1/repos/kastors-1.1/"
-tools=("skeleton"
+tools=(
+       "skeleton"
        "event_counter" 
        "ancestry_tracker"
       )
 
 # Build KASTORS with no tool linked
-./build_kastors.sh
+${project_root}/build_scripts/build_kastors.sh
 
 # Build KASTORS with each tool listed
 for tool in ${tools[@]}
 do
-    ./build_kastors"_"${tool}".sh"
+    ${project_root}/build_scripts/build_kastors"_"${tool}".sh"
 done
